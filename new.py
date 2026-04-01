e0d35b47f1ecdf724772f4701f60a4588a7294f9
dumbass-sudo <ayushk190524@gmail.com>
2026-04-01 04:05:14 -0700

    new project-1

---
+++ new.py
@@ -0,0 +1,35 @@
+user_choice = input("What?: ")
+if user_choice == "A" or user_choice == "a":
+    print("You will always be happy")
+elif user_choice == "B" or user_choice == "b" or user_choice == "SKIBIDI TOILET IS GOOD" or user_choice == "skibidi toilet is good":
+    print("You will be dum")
+elif user_choice == "C" or user_choice == "c" or user_choice =="Pls i want to learn to play":
+    print("You are trash at everything")
+elif user_choice == "D" or user_choice =="d" or user_choice == "pls i want girls":
+    print("NO GIRLS HAHAHAHAHA")
+elif user_choice == "AAAAAAAAAAAAAAAA" or user_choice == "aaaaaaaaaaaaaaaa" or user_choice == "aA":
+    print("Congrats, you wrote the most dumbest thing but still got it right")
+elif user_choice == "Hi im gay.":
+    print("What? Ew.")
+elif user_choice == "Im not gay.":
+    print("Good for u.")
+elif user_choice == "are you single?":
+    print("Nah i have a gf unlike your single ass")
+elif user_choice == "Bro pls can i have your gf?":
+    print("nah f off she out of your league dumbass.")
+elif user_choice == "Hi" or user_choice =="hi":
+    print("What.... You alive or dead? Cuz im just some program a dum coder wrote cuz he was bored.")
+elif user_choice == "Can i please be your friend?" or user_choice == "can i pls be your friend?" or user_choice == "can i please be your friend" or user_choice == "can i pls be your friend" or user_choice == "can i pls be ur frnd":
+    print("I got better work to answer people than your lonely ass. womp womp.")
+elif user_choice == "bro pls be kind" or user_choice == "bro pls dont be harsh to me" or user_choice == "bro please be kinder":
+    print("nah. just shut up annoying lil ahh. cant swear because the coder was bored out of his mind. so womp womp.")
+elif user_choice == "hey.. pls dont say like that" or user_choice == "bro pls dont say like that" or user_choice == "hey please dont say bad stuff like that" or user_choice == "bro pls no be angy":
+    print("no. womp womp.")
+elif user_choice == "201i" :
+    print("stfu stop wasting my time.")
+elif user_choice == "whats the coders name?":
+    print("why do you want to know that? oh well. his name is called SIKE YOU THOUGHT ILL SAY IT?")
+elif user_choice == "bro im bored." or user_choice == "im bored" or user_choice == "i am bored" or user_choice == "i'm bored" or user_choice == "i bored":
+    print("womp womp. sleep or die idc. jst shut your annoying ahh up for now.")
+elif user_choice == "bro can i have your number?" or user_choice == "can i have your number?" or user_choice == "bro can i have ur number?" or user_choice == "can i have ur number?":
+    print("No you freak. Ask my coder for it. Oh hold up. You dont have the guts to ask him. Womp womp. go cry in the corner LOL")
