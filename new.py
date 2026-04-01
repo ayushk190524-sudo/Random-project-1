e8c440e62bf511eef0c0903ffc8824940976b6a4
dumbass-sudo <ayushk190524@gmail.com>
2026-04-01 08:42:24 -0700

    THIS IS A v2, the other v2 thingy dont work
    WARNING, THERES MORE COMING, IM UPDATING THEM SLOWLY

--- new.py
+++ new.py
@@ -5,13 +5,12 @@ elif user_choice == "B" or user_choice == "b" or user_choice == "SKIBIDI TOILET 
     print("You will be dum")
 elif user_choice == "C" or user_choice == "c" or user_choice =="Pls i want to learn to play":
     print("You are trash at everything")
-elif user_choice == "D" or user_choice =="d" or user_choice == "pls i want girls":
-    print("NO GIRLS HAHAHAHAHA")
+elif user_choice == "D" or user_choice =="d" or user_choice == "pls i want girls" or user_choice == "can u be my gf?":
+    print("NO GIRLS HAHAHAHAHA YOU AINT GETTING ANY GIRL LOSER")
 elif user_choice == "AAAAAAAAAAAAAAAA" or user_choice == "aaaaaaaaaaaaaaaa" or user_choice == "aA":
     print("Congrats, you wrote the most dumbest thing but still got it right")
 elif user_choice == "Hi im gay.":
-    print("What? Ew.")
-elif user_choice == "Im not gay.":
+    print("What? Ew.") # MADE ORIGINAL COPYRIGHTED
     print("Good for u.")
 elif user_choice == "are you single?":
     print("Nah i have a gf unlike your single ass")
@@ -21,15 +20,30 @@ elif user_choice == "Hi" or user_choice =="hi":
     print("What.... You alive or dead? Cuz im just some program a dum coder wrote cuz he was bored.")
 elif user_choice == "Can i please be your friend?" or user_choice == "can i pls be your friend?" or user_choice == "can i please be your friend" or user_choice == "can i pls be your friend" or user_choice == "can i pls be ur frnd":
     print("I got better work to answer people than your lonely ass. womp womp.")
+    #MADE BY Dumbass-sudo
 elif user_choice == "bro pls be kind" or user_choice == "bro pls dont be harsh to me" or user_choice == "bro please be kinder":
     print("nah. just shut up annoying lil ahh. cant swear because the coder was bored out of his mind. so womp womp.")
 elif user_choice == "hey.. pls dont say like that" or user_choice == "bro pls dont say like that" or user_choice == "hey please dont say bad stuff like that" or user_choice == "bro pls no be angy":
     print("no. womp womp.")
 elif user_choice == "201i" :
     print("stfu stop wasting my time.")
+    # NOT MEANT FOR COPY
 elif user_choice == "whats the coders name?":
     print("why do you want to know that? oh well. his name is called SIKE YOU THOUGHT ILL SAY IT?")
 elif user_choice == "bro im bored." or user_choice == "im bored" or user_choice == "i am bored" or user_choice == "i'm bored" or user_choice == "i bored":
     print("womp womp. sleep or die idc. jst shut your annoying ahh up for now.")
 elif user_choice == "bro can i have your number?" or user_choice == "can i have your number?" or user_choice == "bro can i have ur number?" or user_choice == "can i have ur number?":
     print("No you freak. Ask my coder for it. Oh hold up. You dont have the guts to ask him. Womp womp. go cry in the corner LOL")
+a = input('Wanna continue bbg? (y/n): ')
+if a == "y" or a == "Y":
+    print("F U")
+    input("WHAT NOW? FUN FACT, DOESNT MATTER IM CLOSING MYSELF:")
+    print("BYE LOSER")
+
+elif a == "n" or a == "N":
+    print("finally get out loser")
+
+input("still here? this is a ending thingy or whatever, this was made by a lazy and bored coder (DUDE TEENAGER LIKE THIS IS MADE BY A ACTUAL TEENAGER shame on you if you copy all these codes, if you learn from this, well n good, if u copying this, shame on you) so idc what you say bout him im still closing myself, later loser:")
+print("bye")
+
+input("WHAT RU DOING STILL HERE DUMAHH?? I TOLD U THIS IS ALREADY ENDING THINGY, GO OUT AND TOUCH SOME GRASS LOSER, CHAT RESTRICTION = 0 : ")
